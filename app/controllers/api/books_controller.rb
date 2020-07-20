6525cba421d88b4c43b648b4e5578e4ef7aaa52f
Mark <markandrewhamm@gmail.com>
2020-07-20 13:04:17 -0500

    RESTful routes

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,2 +1,38 @@
 class Api::BooksController < ApplicationController
+  def index
+    @books = Book.all
+    render 'index.json.jb'
+  end
+
+  def show
+    @book = Book.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
+  def create
+    @book = Book.create(
+      name: params[:name],
+      isbn: params[:isbn],
+      genre: params[:genre]
+    )
+    @book.save
+    render 'show.json.jb'
+  end
+
+  def update
+    @book = Book.find_by(id: params[:id])
+    @book.update(
+      name: params[:name] || @book.name,
+      isbn: params[:isbn] || @book.isbn,
+      genre: params[:genre] || @book.genre
+    )
+    @book.save
+    render 'show.json.jb'
+  end
+
+  def destroy
+    @book = Book.find_by(id: params[:id])
+    @book.destroy
+    render json: {message: "The book has been deleted"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/books" => "books#index"
+    get "/books/:id" => "books#show"
+    post "/books" => "books#create"
+    patch "/books/:id" => "books#update"
+    delete "/books/:id" => "books#destroy"
+  end
 end
